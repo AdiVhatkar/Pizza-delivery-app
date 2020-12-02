7502163f6c79f77b1fb613da88daa211b7bf315b
“Adivhatkar” <adivhatkar@gmail.com>
2020-12-02 08:18:56 +0100

    updating pizza app

---
+++ app/http/controllers/authController.js
@@ -0,0 +1,97 @@
+
+const User = require('../../models/user')
+const bcrpt = require('bcrypt')
+const passport = require('passport')
+function authController() {
+    const _getRedirectUrl = (req) => {
+        return req.user.role === 'admin' ? '/admin/orders' : '/customer/orders'
+    }
+
+    return {
+        login(req, res) {
+            res.render('auth/login')
+
+        },
+        register(req, res) {
+            res.render('auth/register')
+
+        },
+        postLogin(req, res, next) {
+
+            const { email, password } = req.body
+            // validate request
+            if ( !email || !password) {
+                req.flash('error', 'All fields are required')
+                return res.redirect('/login')
+            }
+
+            passport.authenticate('local', (err, user, info) => {
+                if (err) {
+                    req.flash('error', info.message)
+                    return next(err)
+                }
+                if (!user) {
+                    req.flash('error', info.message)
+                    return res.redirect('/login')
+                }
+                req.login(user, (err) => {
+                    if (err) {
+                        req.flash('error', info.message)
+                        return next(err)
+                    }
+                    return res.redirect( _getRedirectUrl(req))
+                })
+            })(req, res, next)
+        },
+
+
+
+        async postRegister(req, res) {
+            const { name, email, password } = req.body
+            // validate request
+            if (!name || !email || !password) {
+                req.flash('error', 'All fields are required')
+                req.flash('name', name)
+                req.flash('email', name)
+                return res.redirect('/register')
+            }
+
+            //  check if email exist 
+            User.exists({ email: email }, (err, result) => {
+                if (result) {
+                    req.flash('error', 'Email already exist')
+                    req.flash('name', name)
+                    req.flash('email', name)
+                    return res.redirect('/register')
+
+                }
+            })
+            //  Hash Password
+
+            const hashedPassword = await bcrpt.hash(password, 10)
+            // create a user 
+            const user = new User({
+                name: name,
+                email: email,
+                password: hashedPassword
+            })
+            user.save().then((user) => {
+                //Login 
+                return res.redirect('/')
+            }).catch(err => {
+                req.flash('error', 'Something went Wrong')
+                return res.redirect('/register')
+
+
+            })
+            console.log(req.body)
+        },
+
+        logout(req,res){
+            req.logout()
+            return res.redirect('/login')
+        }
+
+    }
+}
+module.exports = authController
--- server.js
+++ server.js
@@ -1,19 +1,95 @@
+
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 
-app.get('/', (req, res) => {
-    res.render('home')
-  })
+// database connection 
+
+mongoose.connect("mongodb://localhost:27017/pizza", {
+useNewUrlParser: "true", useCreateIndex: "true", 
+useUnifiedTopology: "true", useFindAndModify: "true"
+})
+mongoose.connection.on("error", err => {
+  console.log("err", err)
+})
+mongoose.connection.on("connected", (err, res) => {
+  console.log("mongoose is connected")
+})
+
+
+//session store 
+let mongoStore = new MongoDbStore({
+  mongooseConnection: mongoose.connection,
+  collection:'sessions'
+})
+
+//session configure 
+
+app.use(session({
+secret: process.env.COOKIE_SECRET,
+resave:false,
+store: mongoStore,
+saveUninitialized:false,
+cookie:{maxAge:1000 * 60 * 60 * 24}//24 hours
+// cookie:{maxAge:1000 *10}
+  }))
+
+// passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash())
+
+// Assets
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// Global middleware
+app.use((req, res, next) => {
+res.locals.session = req.session
+res.locals.user = req.user
+next()
+})
+
 
 // set Template Engine 
 app.use(expressLayout)
 app.set('views', path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
-app.listen(PORT,(res,req)=>{
+require('./routes/web')(app)
+
+
+ app.listen(PORT,(res,req)=>{
 console.log("listening on port 3000")
 })
+
+// Socket
+
+// const io = require('socket.io')(server)
+// io.on('connection', (socket) => {
+//       // Join
+//       socket.on('join', (orderId) => {
+//         socket.join(orderId)
+//       })
+// })
+
+// eventEmitter.on('orderUpdated', (data) => {
+//     io.to(`order_${data.id}`).emit('orderUpdated', data)
+// })
+
+// eventEmitter.on('orderPlaced', (data) => {
+//     io.to('adminRoom').emit('orderPlaced', data)
+// })
